a714ed07b0ae61ecf3410c68f3e34dd427fc6eea
Martin Magnusson <martin.h.magnusson@gmail.com>
2026-03-12 12:59:31 +0100

    axes7

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ export function App() {
       left: Math.max(40, Math.round(imageWidth * 0.045)),
       right: Math.max(10, Math.round(imageWidth * 0.012)),
       top: Math.max(8, Math.round(imageHeight * 0.01)),
-      bottom: Math.max(40, Math.round(imageHeight * 0.045))
+      bottom: Math.max(72, Math.round(imageHeight * 0.08))
     }),
     [imageWidth, imageHeight]
   );
